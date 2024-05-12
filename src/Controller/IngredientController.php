bebed8112cd5f4e321871461854f27da95d4e4ef
Rabearivony Loïc <122444363+space9cowboy@users.noreply.github.com>
2024-05-12 20:44:51 +0200

    create ingredientcontroller and display them

---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\IngredientRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class IngredientController extends AbstractController
+{
+    /**
+     * Cette fonction permet d'afficher tout les ingrédients
+     *
+     * @param IngredientRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/ingredient', name: 'ingredient', methods: ['GET']) ]
+    public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
+    {
+        $ingredients = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
+        
+        return $this->render('pages/ingredient/index.html.twig', [
+            'ingredients' => $ingredients
+        
+        ]);
+    }
+}
